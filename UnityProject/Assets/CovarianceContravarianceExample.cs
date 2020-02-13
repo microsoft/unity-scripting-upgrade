4aacebf09333888d1e87225996db37274b92e9d9
Hyunsoo Kim <hot4133@gmail.com>
2020-02-13 09:32:43 +0900

    Divide and add a covariance example. (#1)

--- UnityProject/Assets/CovarianceContravarianceExample.cs
+++ UnityProject/Assets/CovarianceContravarianceExample.cs
@@ -5,10 +5,22 @@ public class CovarianceContravarianceExample : MonoBehaviour
 {
     private void Start()
     {
-        Action<Base> baseAction = (target) => { Debug.Log(target.GetType().Name + " covariance / contravariance worked"); };
+        Action<Base> baseAction = (target) => { Debug.Log(target.GetType().Name + " contravariance worked with a parameter."); };
         Action<Derived> derivedAction = baseAction;
-        derivedAction(new Derived());        
+        derivedAction(new Derived());
+
+        Func<Derived> derivedFunc = () =>
+        {
+            var derived = new Derived();
+            Debug.Log(derived.GetType().Name + " covariance worked with a return value.");
+            return derived;
+        };
+
+        Func<Base> baseFunc = derivedFunc;
+        baseFunc();
     }
 }
+
 public class Base { }
+
 public class Derived : Base { }
